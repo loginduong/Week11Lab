e6837715bb2f708b470d6413457cbaf7a163fc8a
awarsyle <awarsyle@SAIT224519IT.ACDM.DS.SAIT.CA>
2020-10-28 14:34:36 -0600

    Added a cookie util and save the email in a cookie

--- src/java/servlets/LoginServlet.java
+++ src/java/servlets/LoginServlet.java
@@ -2,12 +2,14 @@ package servlets;
 
 import java.io.IOException;
 import javax.servlet.ServletException;
+import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 import models.User;
 import services.AccountService;
+import utilities.CookieUtil;
 
 public class LoginServlet extends HttpServlet {
 
@@ -16,6 +18,10 @@ public class LoginServlet extends HttpServlet {
             throws ServletException, IOException {
         HttpSession session = request.getSession();
         session.invalidate(); // just by going to the login page the user is logged out :-) 
+        
+        Cookie[] cookies = request.getCookies();
+        String email = CookieUtil.getCookieValue(cookies, "email");
+        request.setAttribute("email", email);
         
         getServletContext().getRequestDispatcher("/WEB-INF/login.jsp").forward(request, response);
     }
@@ -26,11 +32,17 @@ public class LoginServlet extends HttpServlet {
         String email = request.getParameter("email");
         String password = request.getParameter("password");
         
+        // save email to a cookie
+        Cookie cookie = new Cookie("email", email);
+        cookie.setMaxAge(60 * 60 * 24 * 365 * 3);
+        response.addCookie(cookie);
+        
         AccountService as = new AccountService();
         String path = getServletContext().getRealPath("/WEB-INF");
         User user = as.login(email, password, path);
         
         if (user == null) {
+            request.setAttribute("email", email);
             getServletContext().getRequestDispatcher("/WEB-INF/login.jsp").forward(request, response);
             return;
         }
--- web/WEB-INF/login.jsp
+++ web/WEB-INF/login.jsp
@@ -9,7 +9,7 @@
         <h1>Notes App</h1>
         <h2>Login</h2>
         <form action="login" method="post">
-            email: <input type="text" name="email"><br>
+            email: <input type="text" name="email" value="${email}"><br>
             password: <input type="password" name="password"><br>
             <input type="submit" value="Sign in">
         </form>
